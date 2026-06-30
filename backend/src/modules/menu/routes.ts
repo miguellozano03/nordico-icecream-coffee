cba400c2bfdb83adf0f98d9f1465c587af9f0cf4
miguellozano03 <miguel.lozano.vergara@gmail.com>
2026-06-30 15:38:24 -0500

    feat (validators): add endpoints validations

--- backend/src/modules/menu/routes.ts
+++ backend/src/modules/menu/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { prisma } from "../../config/prisma";
 import { ImageService } from "../../services/image.service";
+import { validate } from "../../middlewares/validate";
 import { upload } from "../../middlewares/upload.middleware";
 import { CategoryService, ProductService } from "./service";
 import { CategoryController, ProductController } from "./controller";
+import {
+  idParamSchema,
+  categoryCreateSchema,
+  categoryUpdateSchema,
+  productCreateSchema,
+  productUpdateSchema,
+} from "./types";
 
 export const router = Router();
 const categoryController = new CategoryController(new CategoryService(prisma));
@@ -15,13 +23,38 @@ const productController = new ProductController(
 // Category controller
 
 router.get("/categories", categoryController.getAll);
-router.post("/categories", categoryController.create);
-router.patch("/categories/:id", categoryController.update);
-router.delete("/categories/:id", categoryController.delete);
+router.post(
+  "/categories",
+  validate({ body: categoryCreateSchema }),
+  categoryController.create,
+);
+router.patch(
+  "/categories/:id",
+  validate({ params: idParamSchema, body: categoryUpdateSchema }),
+  categoryController.update,
+);
+router.delete(
+  "/categories/:id",
+  validate({ params: idParamSchema }),
+  categoryController.delete,
+);
 
 // Product controller
 
 router.get("/products", productController.getAll);
-router.post("/products", upload.single("image"), productController.create);
-router.patch("/products/:id", productController.update);
-router.patch("/products/:id", productController.delete);
+router.post(
+  "/products",
+  validate({ body: productCreateSchema }),
+  upload.single("image"),
+  productController.create,
+);
+router.patch(
+  "/products/:id",
+  validate({ params: idParamSchema, body: productUpdateSchema }),
+  productController.update,
+);
+router.patch(
+  "/products/:id",
+  validate({ params: idParamSchema }),
+  productController.delete,
+);
--- backend/src/modules/menu/types.ts
+++ backend/src/modules/menu/types.ts
@@ -1,41 +1,57 @@
+import * as z from "zod";
+
+// params
+
+export const idParamSchema = z.object({
+  id: z.uuid(),
+});
+
 // Users
 
-export interface UserCreate {
-  email: string;
-  name: string;
-  lastname: string;
-}
+export const userCreateSchema = z.object({
+  email: z.email(),
+  name: z.string(),
+  lastname: z.string(),
+});
 
-export interface UserUpdate {
-  email?: string;
-  name?: string;
-  lastname?: string;
-}
+export const userUpdateSchema = z.object({
+  email: z.email().optional(),
+  name: z.string().optional(),
+  lastname: z.string().optional(),
+});
+
+export type UserCreate = z.infer<typeof userCreateSchema>;
+export type UserUpdate = z.infer<typeof userUpdateSchema>;
 
 // Category
 
-export interface CategoryCreate {
-  name: string;
-}
+export const categoryCreateSchema = z.object({
+  name: z.string().min(3),
+});
 
-export interface CategoryUpdate {
-  name?: string;
-}
+export const categoryUpdateSchema = z.object({
+  name: z.string().min(3).optional(),
+});
+
+export type CategoryCreate = z.infer<typeof categoryCreateSchema>;
+export type CategoryUpdate = z.infer<typeof categoryUpdateSchema>;
 
 // Product
 
-export interface ProductCreate {
-  categoryId: string;
-  image?: string;
-  title: string;
-  description: string;
-  price: number;
-}
+export const productCreateSchema = z.object({
+  categoryId: z.string(),
+  title: z.string(),
+  description: z.string(),
+  price: z.coerce.number().positive(),
+});
 
-export interface ProductUpdate {
-  categoryId?: string;
-  image?: string | null;
-  title?: string;
-  description?: string;
-  price?: number;
-}
+export const productCreatePayloadSchema = productCreateSchema.extend({
+  image: z.string().nullable().optional(),
+});
+
+export const productUpdateSchema = productCreateSchema.partial().extend({
+  image: z.string().nullable().optional(),
+});
+
+export type ProductCreate = z.infer<typeof productCreatePayloadSchema>;
+export type ProductUpdate = z.infer<typeof productUpdateSchema>;
